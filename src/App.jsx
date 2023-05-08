7b6fa49b67b93317fe85669d030e7e4859298d9e
biplob <biplobpust10@gmail.com>
2023-05-09 05:29:38 +0600

    'first'

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import './style.css'
+import { NewTodoForm } from './NewTodoForm'
+import { TodoList } from './TodoList'
+
+export default function App()
+{
+  const [todos, setTodos] = useState(()=>{
+    const localValue =localStorage.getItem("items")
+    if(localValue == null) return []
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() =>
+  {
+    localStorage.setItem("items", JSON.stringify(todos))
+  }, [todos])
+  function addTodos(title)
+  {
+    setTodos((currentTodos) =>
+    {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ]
+    })
+  }
+  function toggleTodo(id, completed)
+  {
+    setTodos(currentTodos =>
+    {
+      return currentTodos.map(todo =>
+      {
+        if (todo.id === id)
+        {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+  function deleteTodo(id)
+  {
+    setTodos(currentTodos =>
+    {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
+
+
+  return (
+    <>
+      <NewTodoForm onSubmit={addTodos} />
+      <h1 className='header'>Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </>
+  )
+}
